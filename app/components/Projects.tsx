1314f3bc8867626a3d98160fcdbc471c59fea870
Adla Fayyaz <adlafayyaz@gmail.com>
2026-03-10 21:32:02 +0700

    projects change

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -194,7 +194,7 @@ export default function Projects() {
                     >
                       <path d="M18 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2h6M15 3h6v6M10 14L21 3" />
                     </svg>
-                    Live
+                    Site
                   </a>
                 )}
               </div>
